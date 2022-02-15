04943f0c8c95eec5466b3a0f30380f9b531fc67a
expertwebdev <numan3102000@gmail.com>
2022-02-15 13:27:38 +0530

    new features added

--- src/script.js
+++ src/script.js
@@ -1,19 +1,25 @@
 
 const headerContent = document.querySelector(".header_content");
 const mainBody = document.querySelector("main");
+const footer = document.querySelector("footer");
 const videoPage = document.querySelector(".video_page");
+const notifyPage = document.querySelector(".notification_menu");
 const blogPage = document.querySelector(".blog_page");
 const homeBtn = document.getElementById("homeBtn");
 const blogsBtn = document.getElementById("blogsBtn");
 const videoBtn = document.getElementById("videoBtn");
+const bellIcon = document.getElementById("bellIcon");
 AOS.init();
+var isNotification=false;
 
 homeBtn.addEventListener('click', () => {
     videoPage.style.display = "none";
     mainBody.style.display = "block";
     headerContent.style.display = "flex";
     blogPage.style.display = "none";
-
+    notifyPage.style.display = "none";
+    footer.style.display = "flex";
+    bellIcon.style.fill = "#fff";
 })
 
 blogsBtn.addEventListener('click', () => {
@@ -21,7 +27,8 @@ blogsBtn.addEventListener('click', () => {
     mainBody.style.display = "none";
     headerContent.style.display = "none";
     blogPage.style.display = "flex";
-
+    notifyPage.style.display = "none";
+    bellIcon.style.fill = "#fff";
 })
 
 videoBtn.addEventListener('click', () => {
@@ -29,7 +36,30 @@ videoBtn.addEventListener('click', () => {
     mainBody.style.display = "none";
     headerContent.style.display = "none";
     blogPage.style.display = "none";
-
+    notifyPage.style.display = "none";
+    bellIcon.style.fill = "#fff";
 })
+
+bellIcon.addEventListener('click', () => {
+    if(isNotification==false){
+        videoPage.style.display = "none";
+        mainBody.style.display = "none";
+        headerContent.style.display = "none";
+        blogPage.style.display = "none";
+        notifyPage.style.display = "flex";
+        bellIcon.style.fill = "#f2ff00";
+        isNotification=true
+        footer.style.display = "none";
+    } else {
+        videoPage.style.display = "none";
+        mainBody.style.display = "block";
+        headerContent.style.display = "flex";
+        blogPage.style.display = "none";
+        notifyPage.style.display = "none";
+        bellIcon.style.fill = "#fff";
+        isNotification=false
+    }
+})
+
 
 
