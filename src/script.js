0805f9d3b1ef64a72e9a7b17de342e11340c9b42
expertwebdev <numan3102000@gmail.com>
2022-02-21 23:23:18 +0530

    bug fixed

--- src/script.js
+++ src/script.js
@@ -1,88 +1,92 @@
 
 window.addEventListener("load", () => {
-    document.querySelector(".loader_page").style.display = "none";
-})
+    let loading = document.querySelector(".loader_page");
+    
+    const headerContent = document.querySelector(".header_content");
+    const mainBody = document.querySelector("main");
+    const footer = document.querySelector("footer");
+    const videoPage = document.querySelector(".video_page");
+    const notifyPage = document.querySelector(".notification_menu");
+    const blogPage = document.querySelector(".blog_page");
+    const homeBtn = document.getElementById("homeBtn");
+    const blogsBtn = document.getElementById("blogsBtn");
+    const videoBtn = document.getElementById("videoBtn");
+    const bellIcon = document.getElementById("bellIcon");
+    var loadSite = document.querySelector(".load_site");
+    AOS.init();
+    var isNotification = false;
 
-const headerContent = document.querySelector(".header_content");
-const mainBody = document.querySelector("main");
-const footer = document.querySelector("footer");
-const videoPage = document.querySelector(".video_page");
-const notifyPage = document.querySelector(".notification_menu");
-const blogPage = document.querySelector(".blog_page");
-const homeBtn = document.getElementById("homeBtn");
-const blogsBtn = document.getElementById("blogsBtn");
-const videoBtn = document.getElementById("videoBtn");
-const bellIcon = document.getElementById("bellIcon");
-var loadSite = document.querySelector(".load_site");
-AOS.init();
-var isNotification = false;
-
-homeBtn.addEventListener('click', () => {
-    loadSite.classList.remove("item_hidden");
-    videoPage.style.display = "none";
-    mainBody.style.display = "block";
-    headerContent.style.display = "flex";
-    blogPage.style.display = "none";
-    notifyPage.style.display = "none";
-    footer.style.display = "flex";
-    bellIcon.style.fill = "#fff";
-    loadSite.classList.add("item_hidden");
-})
-
-blogsBtn.addEventListener('click', () => {
-    loadSite.classList.remove("item_hidden");
-    setTimeout(() => {
-        videoPage.style.display = "none";
-        mainBody.style.display = "none";
-        headerContent.style.display = "none";
-        notifyPage.style.display = "none";
-        bellIcon.style.fill = "#fff";
-        footer.style.display = "none";
-        blogPage.style.display = "flex";
-        loadSite.classList.add("item_hidden");
-    }, 1000);
-
-})
-
-videoBtn.addEventListener('click', () => {
-    loadSite.classList.remove("item_hidden");
-    setTimeout(() => {
-        videoPage.style.display = "flex";
-        mainBody.style.display = "none";
-        headerContent.style.display = "none";
-        blogPage.style.display = "none";
-        footer.style.display = "none";
-        notifyPage.style.display = "none";
-        bellIcon.style.fill = "#fff";
-        loadSite.classList.add("item_hidden");
-    }, 1000);
-})
-
-bellIcon.addEventListener('click', () => {
-    if (isNotification == false) {
-        videoPage.style.display = "none";
-        mainBody.style.display = "none";
-        headerContent.style.display = "none";
-        blogPage.style.display = "none";
-        notifyPage.style.display = "flex";
-        bellIcon.style.fill = "#f2ff00";
-        isNotification = true
-        footer.style.display = "none";
-    } else {
+    homeBtn.addEventListener('click', () => {
+        loadSite.classList.remove("item_hidden");
         videoPage.style.display = "none";
         mainBody.style.display = "block";
         headerContent.style.display = "flex";
         blogPage.style.display = "none";
         notifyPage.style.display = "none";
+        footer.style.display = "flex";
         bellIcon.style.fill = "#fff";
-        isNotification = false
-    }
+        loadSite.classList.add("item_hidden");
+    })
+
+    blogsBtn.addEventListener('click', () => {
+        loadSite.classList.remove("item_hidden");
+        setTimeout(() => {
+            videoPage.style.display = "none";
+            mainBody.style.display = "none";
+            headerContent.style.display = "none";
+            notifyPage.style.display = "none";
+            bellIcon.style.fill = "#fff";
+            footer.style.display = "none";
+            blogPage.style.display = "flex";
+            loadSite.classList.add("item_hidden");
+        }, 1000);
+
+    })
+
+    videoBtn.addEventListener('click', () => {
+        loadSite.classList.remove("item_hidden");
+        setTimeout(() => {
+            videoPage.style.display = "flex";
+            mainBody.style.display = "none";
+            headerContent.style.display = "none";
+            blogPage.style.display = "none";
+            footer.style.display = "none";
+            notifyPage.style.display = "none";
+            bellIcon.style.fill = "#fff";
+            loadSite.classList.add("item_hidden");
+        }, 1000);
+    })
+
+    bellIcon.addEventListener('click', () => {
+        if (isNotification == false) {
+            videoPage.style.display = "none";
+            mainBody.style.display = "none";
+            headerContent.style.display = "none";
+            blogPage.style.display = "none";
+            notifyPage.style.display = "flex";
+            bellIcon.style.fill = "#f2ff00";
+            isNotification = true
+            footer.style.display = "none";
+        } else {
+            videoPage.style.display = "none";
+            mainBody.style.display = "block";
+            headerContent.style.display = "flex";
+            blogPage.style.display = "none";
+            notifyPage.style.display = "none";
+            bellIcon.style.fill = "#fff";
+            isNotification = false
+        }
+    })
+
+    loading.style.display = "none";
+
+    let readMoreBtn = document.querySelector("#readMoreBtn");
+    readMoreBtn.addEventListener("click", () => {
+        location.href = "hello.html";
+    })
 })
 
-let readMoreBtn = document.querySelector("#readMoreBtn");
-readMoreBtn.addEventListener("click", () => {
-    alert("Just an alert message");
-})
+
 
 
 
